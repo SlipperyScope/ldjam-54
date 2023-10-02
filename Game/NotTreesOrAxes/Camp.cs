53d2b3e3644f76348e7ab2e1d9f7462c65cb1f9b
Andrew <pXn.Tilde@protonmail.com>
2023-10-01 21:27:24 -0700

    added camp

---
+++ Game/NotTreesOrAxes/Camp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+
+namespace Game.NotTreesOrAxes;
+public partial class Camp : Node2D
+{
+    
+}
